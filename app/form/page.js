6b243b1e4198ec4ca827438bf9482fa627767b64
a-2280 <khaliff.williamss@gmail.com>
2025-08-17 22:02:55 -0600

    fixed netlify form

--- app/form/page.js
+++ app/form/page.js
@@ -7,6 +7,7 @@ export default function Form() {
     <form
       className="flex flex-col gap-2"
       method="POST"
+      action="/form"
       id="form"
       name="contact"
       data-netlify="true"
