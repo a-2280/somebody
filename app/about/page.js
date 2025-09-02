ab172c31781e82f1258fe5851ea70daa454d5a1b
a-2280 <khaliff.williamss@gmail.com>
2025-09-01 20:47:28 -0600

    shortened the about paragraph width

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <p className="text-justify md:max-w-[75%] lg:max-w-[50%]">
+    <p className="text-justify md:max-w-[75%] lg:max-w-[25%]">
       {`I'm a freelance web developer who specializes in building clean,
       responsive websites that help businesses connect with their audiences. I
       pride myself on being easy to work with and turning complex requirements
